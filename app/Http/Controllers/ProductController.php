b0350c2d28eca0a6d4136e53fda6bc2f442c930e
kenshu-arihito <kenshu.arihito@gmail.com>
2024-05-09 14:48:33 +0900

    refact: N+1 Eager

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,20 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Category;
 class ProductController extends Controller
 {
     public function index()
     {
-        // $collection = Product::where('category_id', '=', 1)->where('price', '>=', 10)->where(function ($q)
-        // {
-            //     $q->where('name', 'LIKE', '%チョコ%');
-            //     $q->orWhere('description', 'LIKE', '%チョコ%');
-            // })->get();
-
         return view('index', [
-            'products'   => Product::all(),
-            'categories' => Category::all()
+            'products' => Product::with('category')->get()
         ]);
     }
 }
